e2f50511251155644c3369ffa7ccd00705e6fd32
M1ndForce <66359796+M1ndForce@users.noreply.github.com>
2021-01-02 21:39:41 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        COLOR
+        <br />
+        <span id="colorDisplay">RGB</span>
+        <br />
+        GAME
+      </h1>
+
+      <div id="stripe">
+        <button id="reset">New Colors</button>
+        <span id="message" />
+        <button id="easyBtn">Easy</button>
+        <button id="hardBtn" className="selected">
+          Hard
+        </button>
+      </div>
+
+      <div id="container">
+        <div className="square" />
+        <div className="square" />
+        <div className="square" />
+        <div className="square" />
+        <div className="square" />
+        <div className="square" />
+      </div>
     </div>
   );
 }
